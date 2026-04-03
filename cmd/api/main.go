38f1a4c3ce964aac4a3e7670b9f453ec6b04b342
faizfajar <faizaprianda112@gmail.com>
2026-04-03 17:53:03 +0700

    feat: add cors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -38,6 +38,19 @@ func main() {
 
 	// Setup Gin Router with
 	r := gin.New()
+
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Account-Type")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
+
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 
